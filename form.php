dea0fdc9cb4ea59f2681359f469e40b70b2e22e6
minudaSaswitha <sajithgotabaya@gmail.com>
2022-08-26 02:53:22 +0530

    Initial commit

---
+++ form.php
@@ -0,0 +1,31 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reg_form1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$school = $_POST["school_name"];
+$name = $_POST["name"];
+$num = $_POST["phone_num"];
+$date = $_POST["birth_day"];
+
+$sql = "INSERT INTO details (school,namee,numberr,birthday) VALUES ('$school','$name','$num','$date')";
+
+
+if ($conn->query($sql) === TRUE) {
+  header("Location: ");
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+?>
